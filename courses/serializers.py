e81c5af4540a2d990b3504f341ffc7f483b1a768
kuzin_sn <kuzinnatra@yandex.ru>
2024-12-15 09:29:31 +0400

    25_2 выполнено Задание_1

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from courses.models import Cours, Lesson
+from courses.validators import YoutubeValidators
 
 
 class LessonSerializer(ModelSerializer):
+    validators = [YoutubeValidators(field='video_url')]
     class Meta:
         model = Lesson
         fields = "__all__"
